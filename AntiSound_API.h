bd675349f621edda96a0024777a40fa394ab3c65
AntiSoundWorld <AntiSoundWorld@gmail.com>
2020-10-18 23:04:15 +0300

    test

--- AntiSound_API.h
+++ AntiSound_API.h
@@ -1,5 +1,4 @@
 # include "AntiSound_HTTP.h"
 
-void antiSound_api_start();
-void antiSound_api_request();
+void antisound_api_start();
 void antiSound_api_response();
---
+++ Task.h
@@ -0,0 +1,6 @@
+typedef struct Task
+{
+    char* id;
+    char* name;
+    struct Task* nextTask;
+}task_t;
