3b6e6cd5b2c1300a233bc71a7b84af298b1f01cc
AntiSoundWorld <AntiSoundWorld@gmail.com>
2020-10-30 00:02:51 +0300

    remove files and add struct HTTP

--- AntiSound_HTTP/AntiSound_HTTP.h
+++ AntiSound_HTTP/AntiSound_HTTP.h
@@ -1,21 +1,16 @@
 #include <stdbool.h>
 
-
-typedef struct Parametrs
-{
-    char* request;
-    char* method;
-    char* isloatedParametrs;
-    char* isolatedParametrId;
-    char* isolatedParametrName;
-}parametrs_t;
-
 typedef struct Request
 {
-    char* id;
-    char* name;
-    parametrs_t* _parametrs;
+    char* method;
+    http_t* http;
 }request_t;
+
+typedef struct HTTP
+{
+    int minor;
+    int major;
+}http_t;
 
 typedef struct Response
 {
@@ -24,24 +19,4 @@ typedef struct Response
 
 request_t* antiSound_http_parseRuqest(char* request);
 
-void _antiSound_http_isolateParametrs(request_t* structOfRequest);
-void _antiSound_http_isolateParametrId(request_t* structOfRequest);
-void _antiSound_http_isolateParametrName(request_t* structOfRequest);
 
-void _antiSound_http_parseMethod(request_t* structOfRequest);
-void _antiSound_http_parseId(request_t* structOfRequest);
-void _antiSound_http_parseName(request_t* structOfRequest);
-
-bool _antiSound_http_checkSeparatorExist(request_t* structOfRequest);
-bool _antiSound_http_checkLabelIdExist(request_t* structOfRequest);
-bool _antiSound_http_checkParametrIdEqualesExist(request_t* structOfRequest);
-bool _antiSound_http_checkDataIdExist(parametrs_t* parametrs);
-bool _antiSound_http_checkIdExist(request_t* structOfRequest);
-bool _antiSound_http_checkParametrsIdExist(request_t* structOfRequest);
-bool _antiSound_http_checkParametrNameEqualesExist(request_t* structOfRequest);
-bool _antiSound_http_checkLabelNameExist(request_t* structOfRequest);
-bool _antiSound_http_checkDataNameExist(request_t* structOfRequest);
-bool _antiSound_http_checkParametrsNameExist(request_t* structOfRequest);
-bool _antiSound_http_checkNameExist(request_t* structOfRequest);
-
-char* antiSound_http_builResponse(response_t* response);
