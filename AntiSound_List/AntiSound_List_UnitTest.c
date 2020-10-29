dc21c1b349df04998ea94ef509ecbce18f0e3b17
AntiSoundWorld <AntiSoundWorld@gmail.com>
2020-10-29 09:32:51 +0300

    add full auto tests of functions

--- AntiSound_List/AntiSound_List_UnitTest.c
+++ AntiSound_List/AntiSound_List_UnitTest.c
@@ -12,8 +12,6 @@ int main()
 
     list_t* list = antiSound_list_testNew();
 
-    antiSound_list_message(1);
-
     antiSound_list_testAdd(list);
 
     antiSound_list_testUpdate(list); 
@@ -21,6 +19,7 @@ int main()
     antiSound_list_testGetItem(list);
 
     antiSound_list_testGetData(list);
+
     antiSound_list_testRemove(list);
 
     antiSound_list_testLength(list);
@@ -29,117 +28,186 @@ int main()
 
 list_t* antiSound_list_testNew()
 {
-    antiSound_list_message(0);
-    printf("< antiSound_list_testNew >\n");
-
     bool status = false;
 
     list_t* list = antiSound_list_new();
 
-    if(list != NULL)
+    if(list->data == NULL) // check first element
     {
         status = true;
     }
 
-    printf("status[%d]\n", status);
-
-    antiSound_list_message(0);
+    if(status == false)
+    {
+        printf("< antiSound_list_testNew >\n");
+        printf("status[%d]\n", status);
+        antiSound_list_message(0);
+    }
 
     return list;
 }
 
 void antiSound_list_testAdd(list_t* list)
 {
-    printf("< antiSound_list_testAdd >\n\n");
-
     bool status = false;
+    bool isLengthCorrect = false;
+    bool isItemDataCorrect = false;
 
     void* data = "name=Dmitry";
 
     int id = antiSound_list_add(list, data);
 
-    if(id != -1)
+    list_t* item = antiSound_list_getItem(list, id);
+    
+    int length = antiSound_list_length(list);
+
+    if(length == item->id + 1)
+    {
+        isLengthCorrect = true;
+    }
+    
+    if(strcmp(item->data, data) == 0)
+    {
+        isItemDataCorrect = true;
+    }
+
+    if(isItemDataCorrect == true && isLengthCorrect == true)
     {
         status = true;
     }
 
-    printf("status[%d]\n", status);
-
-    antiSound_list_message(0);
+    if(status == false)
+    {
+        printf("< antiSound_list_testAdd >\n\n");
+        printf("status[%d]\n", status);
+        antiSound_list_message(0);
+    }
 }
 
 void antiSound_list_testUpdate(list_t* list)
 {
-    printf("< antiSound_list_testUpdate >\n\n");
-
     bool status = false;
+    bool isUpdateSuccess = false;
+    bool isItemDataCorrect = false;
 
     void* newData = "name=Roma";
-    int id = 2;
+    int id = 0;
 
-    status = antiSound_list_update(list, id, newData);
+    isUpdateSuccess = antiSound_list_update(list, id, newData);
 
-    printf("status[%d]\n", status); 
+    list_t* item = antiSound_list_getItem(list, id);
 
-    antiSound_list_message(0);
+    if(strcmp(item->data, newData) == 0)
+    {
+        isItemDataCorrect = true;
+    }
+
+    if(isItemDataCorrect == true && isUpdateSuccess == true)
+    {
+        status = true;
+    }
+
+    if(status == false)
+    {
+        printf("< antiSound_list_testUpdate >\n\n");
+        printf("status[%d]\n", status); 
+        antiSound_list_message(0);
+    }
 }
 
 void antiSound_list_testGetItem(list_t* list)
 {
     bool status = false;
 
-    printf("< antiSound_list_testGetItem >\n\n");
-
-    int id = 3;
+    int id = 0;
     list_t* item = antiSound_list_getItem(list, id);
 
-    if(item != NULL)
+    if(item->id == id)
     {
         status = true;
     }
     
-    printf("status[%d]\n", status);
-
-    antiSound_list_message(0);
+    if(status == false)
+    {
+        printf("< antiSound_list_testGetItem >\n\n");
+        printf("status[%d]\n", status);
+        antiSound_list_message(0);
+    }
 }
 
 void antiSound_list_testGetData(list_t* list)
 {
-    printf("< antiSound_list_testGetData >\n\n");
-
     bool status = false;
 
-    int id = 3;
+    int id = 0;
+    list_t* item = antiSound_list_getItem(list, id);
+
     void* data = antiSound_list_getData(list, id);
 
-    if(data != NULL)
+    if(data != NULL && item->data == data)
     {
         status = true;
     } 
     
-    printf("status[%d]\n", status);
-    antiSound_list_message(0);
+    if(status == false)
+    {
+        printf("< antiSound_list_testGetData >\n\n");
+        printf("status[%d]\n", status);
+        antiSound_list_message(0);
+    }
 }
 
 void antiSound_list_testRemove(list_t* list)
 {
-    printf("< antiSound_list_testRemove >\n\n");
-
     bool status = false;
+    bool isIdOrderCorrect = false;
+    bool isLengthCorrect = false;
+    bool isRemovableItemExist = false;
 
     int id = 1;
 
-    status = antiSound_list_remove(list, id);
+    int lengthBeforeRemove = antiSound_list_length(list);
+    
+    list_t* removableItem = antiSound_list_getItem(list, id);
 
-    printf("status[%d]\n", status);
+    status = antiSound_list_remove(list, removableItem->id);
 
-    antiSound_list_message(0);
+    list_t* item = antiSound_list_getItem(list, removableItem->id);
+    list_t* previousItem = antiSound_list_getItem(list, item->id - 1);
+    list_t* nextItem = antiSound_list_getItem(list, item->id + 1);
+
+    int lengthAfterRemove = antiSound_list_length(list);
+
+    if(lengthBeforeRemove - 1 == lengthAfterRemove)
+    {
+        isLengthCorrect = true;
+    }
+
+    if(removableItem->data == item->data)
+    {
+        isRemovableItemExist = true;
+    }
+
+    if(item->id - 1 == previousItem->id && item->id + 1 == nextItem->id)
+    {
+        isIdOrderCorrect = true;
+    }
+
+    if(isRemovableItemExist == false && isIdOrderCorrect == true && isLengthCorrect == true)
+    {
+        status = true;
+    }
+
+    if(status == false)
+    {
+        printf("< antiSound_list_testRemove >\n\n");
+        printf("status[%d]\n", status);
+        antiSound_list_message(0);
+    }
 }
 
 void antiSound_list_testLength(list_t* list)
 {
-    printf("< antiSound_list_testLength >\n\n");
-
     bool status = false;
 
     int lengthOfList = antiSound_list_length(list);
@@ -149,10 +217,12 @@ void antiSound_list_testLength(list_t* list)
         status = true;
     }
 
-    printf("status[%d]\n", status);
-
-    antiSound_list_message(0);
-
+    if(status == false)
+    {
+        printf("< antiSound_list_testLength >\n\n");
+        printf("status[%d]\n", status);
+        antiSound_list_message(0);
+    }
 }
 
 void antiSound_list_message(int id)
