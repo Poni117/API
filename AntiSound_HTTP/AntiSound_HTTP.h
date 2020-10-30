c7580df605a776130166d70cb6e93048bd63690e
AntiSoundWorld <AntiSoundWorld@gmail.com>
2020-10-30 12:06:01 +0300

    add struct Parameters

--- AntiSound_HTTP/AntiSound_HTTP.h
+++ AntiSound_HTTP/AntiSound_HTTP.h
@@ -1,26 +1,35 @@
 #include <stdbool.h>
 
-typedef struct Request
-{
-    char* method;
-    http_t* http;
-    url_t* url;
-}request_t;
-
 typedef struct HttpVersion
 {
     int minor;
     int major;
 }httpVersion_t;
 
-typedef Url
+typedef struct Url
 {
     char* host;
     char* port;
     char* path;
-    char* queryParametrs;
+    char* queryParameters;
     char* anchor;
 }url_t;
+
+typedef struct Parameters
+{
+    char* parametr;
+    struct Parameters* next;
+}parameters_t;
+
+typedef struct Request
+{
+    char* method;
+    httpVersion_t* http;
+    url_t* url;
+    parameters_t parameters;
+}request_t;
+
+
 
 typedef struct Response
 {
