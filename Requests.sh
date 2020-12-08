a045c078b2705a9490ff3cb9e2811a0dcdaa342c
AntiSoundWorld <antisoundworld@gmail.com>
2020-12-08 16:24:43 +0300

    remove file from Bash

--- Requests.sh
+++ Requests.sh
@@ -1,4 +1,4 @@
-curl 127.0.0.1:8090/tasks
+curl -v 127.0.0.1:8090/tasks
 
 curl -v -X POST -H 'Content-Type: application/json' 127.0.0.1:8090/tasks -d '{"name":"Dmitry", "id":"0", "lastname":"Ivanov"}'
 
